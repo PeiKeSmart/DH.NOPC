c0c2772992d22ed8b5d9b2da8fa61f3b8bf75885
猿人易 <qcjxberin@outlook.com>
2024-08-19 18:16:15 +0800

    添加 OPC UA 驱动及相关配置文件
    新增 `.editorconfig`、`.gitattributes` 和 `.gitignore` 文件，配置编辑器、行尾符号规范和忽略规则。新增 `DH.NOPC.sln` 解决方案文件和 `global.json` 文件，指定 .NET SDK 版本。新增 `LICENSE` 文件，使用 MIT 许可证。新增 `NuGet.config` 文件，配置 NuGet 包源。新增 `Readme.MD` 文件，描述项目。新增 GitHub Actions 工作流文件 `publish-beta.yml`、`publish.yml` 和 `test.yml`，分别用于发布 beta 版本、正式版本和进行构建测试。
    
    新增 `DH.NOpcDa.csproj` 和 `DH.NOpcUa.csproj` 项目文件，配置 OpcDa 和 OpcUa 协议驱动项目。新增 `OpcDaDriver.cs` 和 `OpcDaParameter.cs` 文件，定义 OpcDa 协议驱动及其参数类。新增 `OpcUaDriver.cs` 和 `OpcUaParameter.cs` 文件，定义 OPC UA 驱动及其参数类，实现了打开通道、读取数据、写入数据等功能，并处理会话保持和重新连接逻辑。
    
    在 `Program.cs` 文件中，添加了示例代码，演示如何使用 `OpcUaDriver` 读取和写入数据，并输出结果到控制台。新增 `Test.csproj` 文件，定义了一个新的测试项目，并引用了 `DH.NOpcUa` 项目。

---
+++ DH.NOpcDa/Drivers/OpcDaDriver.cs
@@ -0,0 +1,168 @@
+﻿using System.ComponentModel;
+using NewLife.IoT;
+using NewLife.IoT.Drivers;
+using NewLife.IoT.ThingModels;
+using NewLife.Serialization;
+using NewLife.Xml;
+
+using Opc;
+using Opc.Da;
+using Node = NewLife.IoT.Drivers.Node;
+
+namespace NewLife.OPC.Drivers;
+
+/// <summary>
+/// OpcDa协议
+/// </summary>
+[Driver("OpcDa")]
+[DisplayName("OpcDa协议")]
+public class OpcDaDriver : DriverBase, IDriver
+{
+    private Opc.Da.Server _client;
+    private Int32 _nodes;
+
+    #region 构造
+    /// <summary>
+    /// 销毁时，关闭连接
+    /// </summary>
+    /// <param name="disposing"></param>
+    protected override void Dispose(Boolean disposing)
+    {
+        base.Dispose(disposing);
+
+        _client.TryDispose();
+        _client = null;
+    }
+    #endregion
+
+    #region 方法
+    /// <summary>
+    /// 创建驱动参数对象，可序列化成Xml/Json作为该协议的参数模板
+    /// </summary>
+    /// <returns></returns>
+    public override IDriverParameter CreateParameter(String parameter)
+    {
+        if (parameter.IsNullOrEmpty()) return new OpcDaParameter();
+
+        return parameter.ToXmlEntity<OpcDaParameter>();
+    }
+
+    /// <summary>
+    /// 打开通道。一个OPC设备可能分为多个通道读取，需要共用Tcp连接，以不同节点区分
+    /// </summary>
+    /// <param name="channel">通道</param>
+    /// <param name="parameter">参数</param>
+    /// <returns></returns>
+    public override INode Open(IDevice channel, IDriverParameter parameter)
+    {
+        var pm = parameter as OpcDaParameter;
+
+        if (_client == null)
+        {
+            var uri = new Uri(pm.Address);
+            var url = new URL(uri.AbsolutePath)
+            {
+                Scheme = uri.Scheme,
+                HostName = uri.Host
+            };
+
+            var client = new Opc.Da.Server(new OpcCom.Factory(), url);
+            client.Connect();
+
+            _client = client;
+        }
+
+        Interlocked.Increment(ref _nodes);
+
+        return new Node
+        {
+            Driver = this,
+            Device = channel,
+            Parameter = pm,
+        };
+    }
+
+    /// <summary>
+    /// 关闭设备驱动
+    /// </summary>
+    /// <param name="node"></param>
+    public override void Close(INode node)
+    {
+        if (Interlocked.Decrement(ref _nodes) <= 0)
+        {
+            _client.TryDispose();
+            _client = null;
+        }
+    }
+
+    /// <summary>
+    /// 读取数据
+    /// </summary>
+    /// <param name="node">节点对象，可存储站号等信息，仅驱动自己识别</param>
+    /// <param name="points">点位集合，Address属性地址示例：D100、C100、W100、H100</param>
+    /// <returns></returns>
+    public override IDictionary<String, Object> Read(INode node, IPoint[] points)
+    {
+        var dic = new Dictionary<String, Object>();
+
+        if (points == null || points.Length == 0) return dic;
+        points = points.Where(e => !e.Address.IsNullOrEmpty()).ToArray();
+
+        // 构造参数，准备批量读取
+        var ids = new List<Item>();
+        foreach (var point in points)
+        {
+            var id = new Item
+            {
+                ItemName = point.Address
+            };
+            ids.Add(id);
+        }
+
+        if (ids.Count > 0)
+        {
+            // 批量读取
+            var results = _client.Read(ids.ToArray());
+
+            // 按照点位逐个赋值
+            for (var i = 0; i < results.Length; i++)
+            {
+                var rs = results[i];
+                if (rs != null && rs.Quality == Quality.Good)
+                {
+                    dic[points[i].Name] = rs.Value;
+                }
+            }
+        }
+
+        return dic;
+    }
+
+    /// <summary>
+    /// 写入数据
+    /// </summary>
+    /// <param name="node">节点对象，可存储站号等信息，仅驱动自己识别</param>
+    /// <param name="point">点位</param>
+    /// <param name="value">数值</param>
+    public override Object Write(INode node, IPoint point, Object value)
+    {
+        var val = new ItemValue
+        {
+            ItemName = point.Address,
+            Value = value
+        };
+
+        var result = _client.Write(new[] { val });
+
+        return result;
+    }
+
+    ///// <summary>
+    ///// 控制设备，特殊功能使用
+    ///// </summary>
+    ///// <param name="node"></param>
+    ///// <param name="parameters"></param>
+    ///// <exception cref="NotImplementedException"></exception>
+    //public virtual void Control(INode node, IDictionary<String, Object> parameters) => throw new NotImplementedException();
+    #endregion
+}
